a18da9c9232ab59fc1ef8c8de3c366e545186360
Buba <34367843+seonk94@users.noreply.github.com>
2019-01-15 10:37:58 +0900

    docs

--- config/index.js
+++ config/index.js
@@ -45,7 +45,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: './',
+    assetsPublicPath: '',
 
     /**
      * Source Maps
