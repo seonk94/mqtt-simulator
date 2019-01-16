d791a8c019e21975631ec588f10d8a41b7957dc3
Buba <34367843+seonk94@users.noreply.github.com>
2019-01-16 13:45:55 +0900

    favicon update

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -48,6 +48,9 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     new webpack.DefinePlugin({
       'process.env': require('../config/dev.env')
     }),
+    new CopyWebpackPlugin([
+      { from: './static/favicon.ico' }
+    ]), 
     new webpack.HotModuleReplacementPlugin(),
     new webpack.NamedModulesPlugin(), // HMR shows correct file names in console on update.
     new webpack.NoEmitOnErrorsPlugin(),
